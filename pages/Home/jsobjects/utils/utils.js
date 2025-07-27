0fe14af7ef4a419cc709e6fedbb4e3e8095a1e10
akletsov88 <akletsov88@gmail.com>
2025-07-27 19:28:50 +0000

    System generated commit, initial commit

---
+++ pages/Home/jsobjects/utils/utils.js
@@ -0,0 +1,37 @@
+export default {
+	commaSeparate(passedArray) {
+    if(passedArray.length === 0) {
+			return `''`;
+		} else { 
+		return passedArray.map(e => `'${e}'`).join();	
+		}
+	} ,
+	commaSeparateIn(passedArray) {
+    if(passedArray.length === 0) {
+			return `''`;
+		} else { 
+		return passedArray.map(e => `psr.market LIKE '%${e}%'`).join(` OR `);	
+		}
+	},
+	customWidgetData() {
+		return {
+			"summary": summary.data
+		}
+	},
+	clearInputs() {
+		Input_home_clicks_count.setValue("");
+		Input_home_clicks_pct.setValue("");
+		Input_dropping_odds_count.setValue("");
+		Input_dropping_odds_pct.setValue("");
+		Input_home_league_pos.setValue("");
+		Input_home_league_pos_pct.setValue("");
+		Input_away_league_pos.setValue("");
+		Input_away_league_pos_pct.setValue("");
+		Input_home_form_above.setValue("");
+		Input_home_form_below.setValue("");
+		Input_away_form_above.setValue("");
+		Input_away_form_below.setValue("");
+		Input_min_h2h_away_odds.setValue("");
+		Select_outcome.setSelectedOption("home");
+	}
+}
